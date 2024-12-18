e7a331514c2aa5173edbb006029b4b389ac2d911
suresh-tech007 <sk4896323@gmail.com>
2024-12-18 22:00:47 +0530

    update code jwtToken

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -6,26 +6,22 @@ const sendToken = (user, rememberMe = false, statusCode, res) => {
     ? 7 * 24 * 60 * 60 * 1000 // 7 days in milliseconds
     : 1 * 60 * 60 * 1000; // 1 hour in milliseconds
 
-  // options for cookie
+  // Options for cookie
   const options = {
-
-    expires: new Date(Date.now() + cookieExpireTime),
-    httpOnly: true,
-    domain: 'reddot-1geh.onrender.com',
-    path: '/',
-    secure: process.env.NODE_ENV === 'PRODUCTION',
-    sameSite: process.env.NODE_ENV === 'PRODUCTION' ? 'None' : 'Lax'
+    expires: new Date(Date.now() + cookieExpireTime), // Expiration time
+    httpOnly: true, // Prevent client-side access to the cookie
+    secure: process.env.NODE_ENV === 'PRODUCTION', // Use secure cookies in production
+    sameSite: process.env.NODE_ENV === 'PRODUCTION' ? 'None' : 'Lax', // SameSite policy
+    path: '/', // Cookie available on all routes
+    domain: process.env.NODE_ENV === 'PRODUCTION' ? 'reddot-1geh.onrender.com' : undefined, // Domain for production
   };
 
   res
     .status(statusCode)
-    .cookie("token", token, options)
+    .cookie("token", token, options) // Set the cookie
     .json({  
       success: true,
       user,
-       
-       
-       
     });
 };
 
